f0b7a52d601053318b6af6e48fe4bdc5655032fc
Anwar Hosain <anwar.sarker@bjitgroup.com>
2023-05-20 20:40:48 +0600

    feat: design upload file and dark mode for chat screen

--- src/app/chats/[...chatId]/page.tsx
+++ src/app/chats/[...chatId]/page.tsx
@@ -4,7 +4,7 @@ import WriteMessage from '@/components/Message/WriteMessage';
 
 const page = () => {
   return (
-    <div className="relative w-full min-h-full">
+    <div className="relative w-full min-h-full dark:bg-background-slate dark:text-gray-300">
       <MessageHeader />
       <div className="overflow-x-auto pb-[5rem]">
         <div className="mt-[7rem] px-4">
@@ -12,75 +12,65 @@ const page = () => {
             name="Mahedi Hasan"
             timestamp={1684556154837}
             message="Hello, how are you?"
-            bg="bg-[#F0F4F9]"
             own={false}
           />
           <Message
             name="Mahedi Hasan"
             timestamp={1684556154837}
             message="Hello, how are you?"
-            bg="bg-[#DDFFEC]"
             own
           />
           <Message
             name="Mahedi Hasan"
             timestamp={1684556154837}
             message="Hello, how are you?"
-            bg="bg-[#F0F4F9]"
             own={false}
           />
           <Message
             name="Mahedi Hasan"
             timestamp={1684556154837}
             message="Hello, how are you?"
-            bg="bg-[#DDFFEC]"
             own
           />
           <Message
             name="Mahedi Hasan"
             timestamp={1684556154837}
             message="Hello, how are you?"
-            bg="bg-[#F0F4F9]"
             own={false}
           />
           <Message
             name="Mahedi Hasan"
             timestamp={1684556154837}
             message="Hello, how are you?"
-            bg="bg-[#DDFFEC]"
             own
           />
           <Message
             name="Mahedi Hasan"
             timestamp={1684556154837}
             message="Hello, how are you?"
-            bg="bg-[#F0F4F9]"
             own={false}
           />
           <Message
             name="Mahedi Hasan"
             timestamp={1684556154837}
             message="Hello, how are you?"
-            bg="bg-[#DDFFEC]"
             own
           />
           <Message
             name="Mahedi Hasan"
             timestamp={1684556154837}
             message="Hello, how are you?"
-            bg="bg-[#F0F4F9]"
             own={false}
           />
           <Message
             name="Mahedi Hasan"
             timestamp={1684556154837}
             message="Hello, how are you?"
-            bg="bg-[#DDFFEC]"
             own
           />
         </div>
       </div>
-      <div className="fixed bottom-0 w-full z-10 h-[3.5rem] bg-white">
+      <div className="fixed bottom-0 w-full z-10 h-[3.5rem] bg-white dark:bg-background-slate ">
         <WriteMessage />
       </div>
     </div>
--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -6,15 +6,16 @@ interface MessageProps {
   message?: string;
   timestamp: number;
   name?: string;
-  bg?: string;
   own?: boolean;
 }
 
-const Message: FC<MessageProps> = ({ message, timestamp, name, bg, own }) => {
+const Message: FC<MessageProps> = ({ message, timestamp, name, own }) => {
   return (
     <div
-      className={`w-[320px] ${bg} ${
-        own ? 'float-right' : 'float-left'
+      className={`w-[320px] ${
+        own
+          ? 'float-right bg-[#DDFFEC] dark:bg-[#191C24]'
+          : 'float-left bg-[#F0F4F9] dark:bg-gray-700'
       } rounded-2xl p-3 h-auto mb-4`}
     >
       <div className="text-brand-main">{name}</div>
